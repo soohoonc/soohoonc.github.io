73390c6a964bfdbfd6dd7f8beb28501e4c63b2b6
SooHoon Choi <soohoonchoi@gmail.com>
2023-08-04 19:56:14 -0700

    shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
+
 export default function Home() {
   return (
     <main>
-
+      Home
     </main>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+'use client'
 
-const Navbar = () => {
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link"
+
+export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <NavigationMenu>
+      <NavigationMenuList className="w-screen flex flex-row justify-center gap-4">
+        <NavigationMenuItem>
+          <Link href="/">
+            <div>
+              Soohoon Choi
+            </div>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/experience">
+            <div>
+              Experience
+            </div>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/blogs">
+            <div>
+              Blogs
+            </div>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/projects">
+            <div>
+              Projects
+            </div>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/about">
+            <div>
+              About
+            </div>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   )
 }
-
-export default Navbar
