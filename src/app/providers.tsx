6c334ad992168f86661ac044345ee8f84c169689
SooHoon Choi <soohoonchoi@gmail.com>
2023-12-11 16:47:46 -0800

    remove rust for a sec

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
-import { createFileSystem, type FileSystem } from '@/lib/fs';
+// import { createFileSystem, type FileSystem } from '@/lib/fs';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
@@ -13,11 +13,11 @@ const FileSystemContext = React.createContext<FileSystem | null>(null);
 export function FileSystemProvider({ children }: { children: React.ReactNode }) {
   const [initialFileSystem, setInitialFileSystem] = React.useState<FileSystem | null>(null);
   React.useEffect(() => {
-    async function init() {
-      const fs = await createFileSystem();
-      setInitialFileSystem(fs);
-    }
-    init();
+    // async function init() {
+    //   const fs = await createFileSystem();
+    //   setInitialFileSystem(fs);
+    // }
+    // init();
   }, []);
   return (
     <FileSystemContext.Provider value={initialFileSystem}>{children}</FileSystemContext.Provider>
--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -1,35 +1,65 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub struct File {
-    name: String,
-    size: u32,
-}
+// #[wasm_bindgen]
+// pub enum FileSystemNode {
+//     Directory(Directory),
+//     File(File),
+//     SymLink(SymLink),
+// }
+// #[wasm_bindgen]
+// pub trait File {
+//     name: String,
+//     permission: Integer,
+// }
 
-#[wasm_bindgen]
-pub struct FileSystem {
-    files: Vec<File>,
-}
+// #[wasm_bindgen]
+// pub struct Directory {
+//     name: String,
+//     parent: String,
+//     children: Vec<FileSystemNode>,
+// }
 
-#[wasm_bindgen]
-impl FileSystem {
-    pub fn new() -> FileSystem {
-        FileSystem { files: Vec::new() }
-    }
+// #[wasm_bindgen]
+// pub struct Symlink {
+//     name: String,
+//     link: String,
+// }
 
-    pub fn add_file(&mut self, name: String, size: u32) {
-        self.files.push(File { name, size });
-    }
+// #[wasm_bindgen]
+// pub struct FileSystem {
+//     root: Directory
+// }
 
-    pub fn get_files(&self) -> String {
-        let mut result = String::new();
-        for file in &self.files {
-            result.push_str(&format!("{}: {}\n", file.name, file.size));
-        }
-        result
-    }
+// #[wasm_bindgen]
+// impl FileSystem {
+//     pub fn new() -> FileSystem {
+//         FileSystem { root: Directory::new('', '') }
+//     }
+// }
 
-    pub fn remove_file(&mut self, name: String) {
-        self.files.retain(|file| file.name != name);
-    }
-}
+// #[wasm_bindgen]
+// impl Directory {
+//     pub fn new(dir_name pa) -> Directory {
+//         Directory {
+//             name: dir_name,
+//             parent: parent_directory,
+//             children: Vec<FileSystemNode>::new()
+//         }
+//     }
+
+//     pub fn add_file(&mut self, name: String, size: u32) {
+//         self.files.push(File { name, size });
+//     }
+
+//     pub fn get_files(&self) -> String {
+//         let mut result = String::new();
+//         for file in &self.files {
+//             result.push_str(&format!("{}: {}\n", file.name, file.permission));
+//         }
+//         result
+//     }
+
+//     pub fn remove_file(&mut self, name: String) {
+//         self.files.retain(|file| file.name != name);
+//     }
+// }
