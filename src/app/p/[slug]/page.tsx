1c8304a67395ce7f7d7a189e2ace7841a1655ab5
SooHoon Choi <soohoonchoi@gmail.com>
2024-08-17 17:59:00 -0700

    glob

--- src/app/p/[slug]/page.tsx
+++ src/app/p/[slug]/page.tsx
@@ -1,8 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import glob from 'glob';
+import { remark } from 'remark';
+import html from 'remark-html';
+
 export const dynamicParams = false;
 
 export function generateStaticParams() {
-  const projects = [{ slug: 'test' }, { slug: '8bit' }, { slug: 'reviewr' }, { slug: 'tabnam' }];
-  return projects;
+  const projectFiles = glob.sync('public/p/*.md');
+  return projectFiles.map((file) => {
+    const slug = path.basename(file, '.md');
+    return { slug };
+  });
 }
 
 interface ProjectPageProps {
@@ -11,11 +20,18 @@ interface ProjectPageProps {
   };
 }
 
-const Page = ({ params }: ProjectPageProps) => {
+const Page = async ({ params }: ProjectPageProps) => {
   const { slug } = params;
+  const filePath = path.join(process.cwd(), 'public', 'projects', `${slug}.md`);
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+
+  const processedContent = await remark().use(html).process(fileContent);
+  const contentHtml = processedContent.toString();
+
   return (
     <div>
       <h2>{slug}</h2>
+      {contentHtml}
     </div>
   );
 };
