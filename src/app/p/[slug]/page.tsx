603488906820365563be54c2c68b1ba95bdc1b8e
soohoonchoi <soohoonchoi@gmail.com>
2024-08-18 23:39:04 -0700

    fix build errors

--- src/app/p/[slug]/page.tsx
+++ src/app/p/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import glob from 'glob';
+import { glob } from 'glob';
 import { remark } from 'remark';
 import html from 'remark-html';
 
@@ -22,7 +22,7 @@ interface ProjectPageProps {
 
 const Page = async ({ params }: ProjectPageProps) => {
   const { slug } = params;
-  const filePath = path.join(process.cwd(), 'public', 'projects', `${slug}.md`);
+  const filePath = path.join(process.cwd(), 'public', 'p', `${slug}.md`);
   const fileContent = fs.readFileSync(filePath, 'utf8');
 
   const processedContent = await remark().use(html).process(fileContent);
