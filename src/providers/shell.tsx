da6d3552a063f01104f7cd5275a08979b62ae065
SooHoon Choi <soohoonchoi@gmail.com>
2024-08-22 21:35:34 -0700

    wasm

--- src/providers/shell.tsx
+++ src/providers/shell.tsx
@@ -1,67 +1,65 @@
-
-
 'use client';
 
 import * as React from 'react';
+import init, { Shell as WASMShell } from '@/wasm/pkg';
 
 const initialShellState: ShellState = {
   history: [] as Message[],
   user: 'guest',
   hostname: 'soohoonchoi.com',
-  shell: {} as Shell
-}
+  shell: {} as Shell,
+};
 
 const ShellStateReducer = (state: ShellState, action: ShellStateAction): ShellState => {
   switch (action.type) {
     case 'addHistory':
       return {
         ...state,
-        history: [...state.history, action.payload]
+        history: [...state.history, action.payload],
       };
     case 'clearHistory':
       return {
         ...state,
-        history: []
+        history: [],
       };
     case 'setUser':
       return {
         ...state,
-        user: action.payload
+        user: action.payload,
       };
     case 'setHostname':
       return {
         ...state,
-        hostname: action.payload
+        hostname: action.payload,
       };
     case 'setShell':
       return {
         ...state,
-        shell: action.payload
+        shell: action.payload,
       };
     default:
       return state;
   }
-}
+};
 
 interface ShellContext {
   history: {
     get: () => Message[];
     add: (message: Message) => void;
     clear: () => void;
-  },
+  };
   user: {
     get: () => string;
     set: (user: string) => void;
-  },
+  };
   hostname: {
     get: () => string;
     set: (hostname: string) => void;
-  },
+  };
   execute: (command: string) => string;
 }
 
 const ShellContext = React.createContext<ShellContext | undefined>(undefined);
-
 
 export function useShell() {
   const context = React.useContext(ShellContext);
@@ -77,38 +75,29 @@ export function ShellProvider({ children }: { children: React.ReactNode }) {
     history: {
       get: () => state.history,
       add: (message: Message) => dispatch({ type: 'addHistory', payload: message }),
-      clear: () => dispatch({ type: 'clearHistory' })
+      clear: () => dispatch({ type: 'clearHistory' }),
     },
     user: {
       get: () => state.user,
-      set: (user: string) => dispatch({ type: 'setUser', payload: user })
+      set: (user: string) => dispatch({ type: 'setUser', payload: user }),
     },
     hostname: {
       get: () => state.hostname,
-      set: (hostname: string) => dispatch({ type: 'setHostname', payload: hostname })
+      set: (hostname: string) => dispatch({ type: 'setHostname', payload: hostname }),
     },
     execute: (command: string) => {
       return state.shell.run(command);
-    }
-  }
+    },
+  };
   React.useEffect(() => {
-    async function init() {
-      const shell = {
-        run: (command: string) => {
-          return command;
-        }
-      } as Shell;
-      // const shell = await getShell();
+    async function initialize() {
+      await init();
+      // https://github.com/rustwasm/wasm-bindgen/issues/166
+      const shell = new WASMShell('guest', 'soohoonchoi.com');
       dispatch({ type: 'setShell', payload: shell });
     }
-    init();
+    initialize();
   }, []);
 
-  return (
-    <ShellContext.Provider
-      value={shellContext}
-    >
-      {children}
-    </ShellContext.Provider>
-  );
+  return <ShellContext.Provider value={shellContext}>{children}</ShellContext.Provider>;
 }
