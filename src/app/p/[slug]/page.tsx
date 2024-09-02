bb5563815ee46230c6f6c669bf5bfc47d0531f4e
SooHoon Choi <soohoonchoi@gmail.com>
2024-09-01 22:14:01 -0700

    local changes

--- src/app/p/[slug]/page.tsx
+++ src/app/p/[slug]/page.tsx
@@ -1,12 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 import { glob } from 'glob';
-import { serialize } from 'next-mdx-remote/serialize';
-import remarkHtml from 'remark-html';
 
-import { MarkdownViewer } from '@/components/markdown-viewer'
+// import { MarkdownViewer } from '@/components/markdown-viewer';
 
-// export const dynamicParams = false;
+export const dynamicParams = false;
 
 export function generateStaticParams() {
   const projectFiles = glob.sync('public/p/*.mdx');
@@ -26,14 +24,11 @@ const Page = async ({ params }: ProjectPageProps) => {
   const { slug } = params;
   const filePath = path.join(process.cwd(), 'public', 'p', `${slug}.mdx`);
   const fileContent = fs.readFileSync(filePath, 'utf8');
-  const mdxSource = await serialize(fileContent, {
-    mdxOptions: {
-      remarkPlugins: [remarkHtml],
-      rehypePlugins: [],
-    },
-  })
   return (
-    <MarkdownViewer source={mdxSource} />
+    <div>
+      {fileContent}
+      {/* <MarkdownViewer content={fileContent} /> */}
+    </div>
   );
 };
 
--- src/components/markdown-viewer.tsx
+++ src/components/markdown-viewer.tsx
@@ -1,15 +1,29 @@
-'use client'
+'use client';
 
-import { MDXRemote } from 'next-mdx-remote';
+import React from 'react';
+import remarkHtml from 'remark-html';
+// import { serialize } from 'next-mdx-remote/serialize';
+// import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote';
 
-interface MarkdownViewerProps {
-  source: any;
-}
-
-export const MarkdownViewer = ({
-  source,
-}: MarkdownViewerProps) => {
-  return (
-    <MDXRemote {...source} />
-  );
-}
+// @ts-ignore
+export const MarkdownViewer = ({ content }) => {
+  // const [source, setSource] = React.useState<MDXRemoteSerializeResult<
+  //   Record<string, unknown>,
+  //   Record<string, unknown>
+  // > | null>(null);
+  React.useEffect(() => {
+    // const parseContent = async () => {
+    //   const mdxSource = await serialize(content, {
+    //     mdxOptions: {
+    //       remarkPlugins: [remarkHtml],
+    //       rehypePlugins: [],
+    //     },
+    //   });
+    //   setSource(mdxSource);
+    // };
+    // parseContent();
+  }, [content]);
+  // if (!source) return null;
+  return <div>{content}</div>;
+  // return <MDXRemote {...source} />;
+};
