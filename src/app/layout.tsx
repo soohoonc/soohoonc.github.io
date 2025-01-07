0f6b745126b2531bcb303961deaa1429777d06fd
soohoonchoi <soohoonchoi@gmail.com>
2025-01-06 23:40:51 -0800

    cool windows manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,12 +11,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body>
-        <main>
-          <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-            {children}
-          </ThemeProvider>
-        </main>
+      <body className="m-0 p-0">
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import { Desktop } from '@/components/Desktop';
 
 
 const Page = () => {
-  return (<Desktop />);
+  return (
+    <main className="w-screen h-screen">
+      <Desktop />
+    </main>
+  )
 };
 
 export default Page;
